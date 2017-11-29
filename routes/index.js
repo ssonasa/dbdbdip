ab8f1b9294d19ff0dca4d72457f0abb930feab37
ssonasa\son <ssk316@ajou.ac.kr>
2017-11-29 19:39:38 +0900

    dbdb 시작

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/keyboard', function(req, res){
+  const menu = {
+    "type": 'buttons',
+    "buttons": ["안녕", "메롱"]
+  };
+
+  res.set({
+    'content-type': 'application/json'
+  }).send(JSON.stringify(menu));
+});
+
+
+module.exports = router;
